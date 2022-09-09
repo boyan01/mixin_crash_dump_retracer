ed26ad2bcbd27b0a863dfcdef2eaadc7f3ed06a9
boyan01 <17426470+boyan01@users.noreply.github.com>
2022-09-09 09:51:46 +0800

    fix version extract

--- retracer_app/lib/src/dump_comment_parser.dart
+++ retracer_app/lib/src/dump_comment_parser.dart
@@ -16,11 +16,11 @@ class DumpCommentParseResult {
 
 DumpCommentParseResult? parseDumpComment(String comment) {
   final match =
-      RegExp(r'(?<=^version: ).*', caseSensitive: false).firstMatch(comment);
+      RegExp(r'(?<=^version:).*', caseSensitive: false).firstMatch(comment);
   if (match == null) {
     return null;
   }
-  final version = match.group(0)!;
+  final version = match.group(0)!.trim();
 
   final miniDumpUrlMatch = RegExp(r'(?<=\().+?(?=\))').firstMatch(comment);
   if (miniDumpUrlMatch == null) {
